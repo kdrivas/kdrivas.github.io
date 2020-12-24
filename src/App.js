1268d80194566d1353dc173926fb1b63988f3106
kervyRivas <a20112128@pucp.pe>
2020-12-23 23:35:08 -0500

    testing travis

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 				<h1>Hola mundo</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
