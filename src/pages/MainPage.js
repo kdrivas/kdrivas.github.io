f3aff30ec37e60c46034c22e03ba65e930993f0e
kervyRivas <a20112128@pucp.pe>
2020-12-26 21:02:31 -0500

    start

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import NavBar from './../components/NavBar';
-import Profile from './../components/Profile';
+//import NavBar from './../components/NavBar';
+//import Profile from './../components/Profile';
 
 const MainPage = props => {
 	return (
-		<NavBar>
 			<div>
 				HomePage
 			</div>
-			<Profile></Profile>
 
-		</NavBar>
 	);
 };
 
