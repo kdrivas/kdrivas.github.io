438f3f1889e7356a2e7ffd11982c086690378e08
kervyRivas <a20112128@pucp.pe>
2020-12-26 23:05:34 -0500

    add

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './../components/NavBar';
 import Profile from './../components/Profile';
 import Experience from './../components/Experience';
+import Publications from './../components/Publications';
 import Achievements from './../components/Achievements';
 import Contact from './../components/Contact';
 
