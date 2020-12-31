e11bbf7167a495bb39f4bcc42d29e3f7aaf21ccb
kervyRivas <a20112128@pucp.pe>
2020-12-30 23:13:03 -0500

    more info

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,18 @@ import PropTypes from 'prop-types';
 const About = props => {
 	return (
 		<div>
-			About
+			<div>About</div>
+			<div>
+				<p>
+					Hello, I'm Dante, nice to meet you!. I'm a data scientist with 2 years of experience, I enjoy building and deploying machine learning models. My goal is to construct clear and efficient architectures in production. Also, in my free time I like learning front-end technologies.
+				</p>
+				<p>
+					After graduating from PUCP, I've been working in an insurance company called Rimac where I learned about data engineering and deployment tools.
+				</p>
+				<p>
+					Finaly, here's a list of my skills:
+				</p>
+			</div>
 		</div>
 	);
 };
