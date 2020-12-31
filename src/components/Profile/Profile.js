d1290733cae13e40023d6976ff83250626925661
kervyRivas <a20112128@pucp.pe>
2020-12-30 20:39:58 -0500

    about

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Profile = props => {
 	return (
 		<div>
-			Profile
+			<div>Hi, my name is</div>
+			<div>Dante Rivas</div>
+			<div>I'm a data scientist based on Lima, Peru. I like developing machine learning models and putting them into production. Also, I enjoy building awesome websites and applications</div>
 		</div>
 	);
 };
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './../components/NavBar';
 import Profile from './../components/Profile';
+import About from './../components/About';
 import Experience from './../components/Experience';
 import Publications from './../components/Publications';
 import Achievements from './../components/Achievements';
@@ -10,6 +11,7 @@ const MainPage = props => {
 	return (
 			<NavBar>
 				<Profile/>
+				<About/>
 				<Experience/>
 				<Publications/>
 				<Achievements/>
