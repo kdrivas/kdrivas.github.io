e86849c353fc1baba7eb71df191616217f18a232
kervyRivas <a20112128@pucp.pe>
2021-06-12 12:01:32 -0500

    removing router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import MainPage from './pages/MainPage';
-import {
-	HashRouter as Router
-} from 'react-router-dom';
 import './App.css';
 
 function App() {
 
   return (
-		<Router>
-			<MainPage/>
-		</Router>
+		<MainPage/>
   );
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,10 +3,6 @@ import { MenuItems } from './../../constants/NavBar';
 import Button from './../Button';
 import Hamburguer from './Hamburguer';
 import './NavBar.css';
-import {
-	  Route,
-	  NavLink,
-	} from "react-router-dom";
 
 const handleClick = (setClicked, clicked) => {
 	setClicked(! clicked);
